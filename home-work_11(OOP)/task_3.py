a9fd193c9125ed8118028eb0eece03ea79759fa4
GermanLamonov <stavr1717@gmail.com>
2024-12-12 00:38:36 +0300

    ДЗ-11(update)

--- home-work_11(OOP)/task_3.py
+++ home-work_11(OOP)/task_3.py
@@ -4,6 +4,9 @@ class Car:
         self.type = type_car
         self.year = year
 
+    def __str__(self):
+        return f"Цвет: {self.color}, Кузов: {self.type}, Год: {self.year}"
+
     def start(self):
         print("Автомобиль заведён")
 
@@ -12,19 +15,23 @@ class Car:
 
     def color_auto(self, color):
         self.color = color
-        print(f"Цвет автомобиля: {color}")
+        print(f"Цвет автомобиля изменён на: {color}")
     def type_auto(self, type_car):
         self.type = type_car
-        print(f"Тип автомобиля: {type_car}")
+        print(f"Тип автомобиля теперь: {type_car}")
     def year_auto(self, year):
         self.year = year
         print(f"Год автомобиля: {year}")
 
 
 auto = Car('Синий', 'Универсал', 1996)
+print(auto)
 auto.start()
+auto.disable()
 auto.color_auto('Красный')
 auto.type_auto('Седан')
 auto.year_auto(2000)
-auto.disable()
+print(auto)
+
+
 
